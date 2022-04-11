251a7f1d3dcd77cbbbc1c2c81598058ef062fd3f
AndrewSavinchuk <savawise@gmail.com>
2022-04-10 20:59:15 -0500

    us 09 runner removed

--- src/test/java/com/tryCloud/runners/CukesRunner.java
+++ src/test/java/com/tryCloud/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/tryCloud/step_definitions",
         dryRun = false,
-        tags = "@CY19-164",
+        tags = "",
         publish = false
 
 )
